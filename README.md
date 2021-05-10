43160816957f5273e14aecfd722de7270b106b9b
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-05-10 16:27:51 +0200

    all 2
    all 2

--- README.md
+++
@@ -1,2 +0,0 @@
-# TyroPlug
-Workspace du TyroPlugin [TEST]
---
+++ tyroliumplugin/src/fr/maxime/tyrolium/plugin/plug.java
@@ -0,0 +1,20 @@
+package fr.maxime.tyrolium.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class plug extends JavaPlugin{
+	
+		public plug() {
+		}
+
+	    @Override
+	    public void onEnable() {
+	        this.getLogger().info("Plugin démarré !");
+	    }
+
+	    @Override
+	    public void onDisable() {
+	        this.getLogger().info("Plugin arreté !");
+	    }
+	
+}
